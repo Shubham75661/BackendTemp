a14349a1abc62a79d37085a87dda0cdf2e5cb842
Shubham <shubham75661@gmail.com>
2025-01-04 11:42:22 +0530

    #ADDED backend dat

--- server.js
+++ server.js
@@ -5,19 +5,42 @@ const app = express();
 
 app.use(express.json());
 
-app.use(cors())
+var corsOptions = {
+    origin: "http://localhost:3000"
+  };
+
+app.use(cors(corsOptions))
 
 let API_VISITS = 0;
 
+let taskArray = ['1st Task']
+
 app.get('/', (req, res)=>{
     API_VISITS++;
-    res.status(200).send({API_VISITS});
+    res.status(200).send(taskArray);
 })
 
-app.post('/user', (req, res) =>{
-    const {username, Age} = req.body;
-    console.log(username, " ", Age);
-    res.send({API_VISITS})
+app.post('/addTask', (req, res) =>{
+    const {task} = req.body;
+    console.log(task, " ");
+    taskArray.push(task)
+    res.status(200)
+})
+
+app.post('/removetask', (req, res) =>{
+    const {task} = req.body;
+    const index = taskArray.indexOf(task);
+    if(index > -1){
+        taskArray.splice(index, 1);
+    }
+})
+
+app.post('/editTask', (req, res) =>{
+    const {task} = req.body;
+    const index = taskArray.indexOf(task);
+    if(index > -1){
+       taskArray[index] = task;
+    }
 })
 
 app.listen(3001, () =>{
